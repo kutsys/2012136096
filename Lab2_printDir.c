ec3a3787be8ef23c122342111ef412f3c8401eb0
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-11-01 10:28:52 +0900

    Update Lab2_printDir.c

--- Lab2_printDir.c
+++ Lab2_printDir.c
@@ -1,50 +1,46 @@
-// printDir.c
-
 #include <unistd.h>
 #include <stdio.h>
-#include <dirent.h>  
+#include <dirent.h>
 #include <string.h>
-#include <sys/stat.h>  
+#include <sys/stat.h>
 #include <stdlib.h>
 
- 
-void printdir(char *dir,int depth)
+void printdir(char *dir, int depth)
 {
-     DIR *dp;  // 디렉토리 포인터 변수 dp 선언
-     struct dirent *entry; // 파일 또는 디렉토리가 가지고 있는 정보 구조체인
-                            // dirent 변수인 포인터 entry 생성
-     struct stat statbuf; // 파일 정보를 담는 구조체 stat버퍼 변수 선언
+	DIR *dp; // 디렉토리 포인터변수 dp 선언
+	struct dirent *entry; // 파일, 또는 디렉토리가 가지고 있는 정보 구조체인// dirent 변수의 포인터 entry 선언
+	struct stat statbuf; // 파일 정보를 담는 stat구조체를 하나 생성한다.
 
-     if((dp=opendir(dir)) == NULL) // 디렉토리 오픈
-     {
-         fprintf(stderr,"cannot open directory : %s \n ",dir);
-         return;
-     } // 열지 못하면 에러메세지를 콘솔에 띄워줌
-     chdir(dir); // 지정한 디렉터리 dir을 현재 디렉터리로 설정
- 
-     while((entry = readdir(dp)) !=NULL)  {
-     //  dp가 가리키는 항목에 대한 정보 가져오기
-        lstat(entry->d_name,&statbuf); // 디렉토리에 대한 정보 저장
-        if(S_ISDIR(statbuf.st_mode)) { // 현재 항목이 디렉터리이고
-            if(strcmp(".",entry->d_name) == 0 ||   //현재 또는 부모 디렉터리면
-              strcmp("..",entry->d_name) ==0) continue; // 그냥 넘어가기
-
-            if ( entry->d_name[0] == '.' ) { }
-            else {
-                print("%s*s%s\n", depth, "", entry->d_name);
-            }
-            printdir(entry->d_name, depth+4);
-        }
-     }
-     chdir("..");
-     closedir(dp);
+	if ( (dp = opendir(dir)) == NULL) { // 디렉토리를 열 수 없을 경우를 점검한다
+		fprintf(stderr, "Cannot Open Directory!!: %s\n", dir);
+		return;
+	}
+	chdir(dir); // 점검 후 지정한 디렉터리를 현재 디렉터리로 만든다.
+	while ((entry = readdir(dp)) != NULL) { // dp가 가리키는 항목에 대한 정보 가져오기
+		lstat(entry->d_name, &statbuf);
+		if (S_ISDIR(statbuf.st_mode)) {
+			if(strcmp(".", entry->d_name) == 0 || 
+				strcmp("..", entry->d_name) == 0)
+				continue;
+			if ( entry->d_name[0] == '.') {  }
+			else {
+				printf("%*s%s\n", depth,"",entry->d_name);
+			}
+		//	printf("%*s%s/\n", depth, "", entry->d_name);
+			printdir(entry->d_name, depth+4);
+		}
+		//else if (strncmp(".", entry->d_name, 1)) {
+		//	printf("%*s%s\n", depth,"",entry->d_name);
+		//}
+	}
+	chdir("..");
+	closedir(dp);
 }
 
- 
-int main()
-{
-	printf("Directory scan of /home/wooyo : \n");
-	printdir("/home/wooyo",0); //함수호출
+int main() {
+	printf("Directory scan of /home/wooyo:\n");
+	printdir("/home/wooyo", 0);
 	printf("done.\n");
- 	exit(0);
-} 
+
+	exit(0);
+}
