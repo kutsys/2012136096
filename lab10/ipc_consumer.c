b882db734e76738a64d0e0548dc92fbb342f1093
KoreaTech-System Programming 2016 <gkgkgy@koreatech.ac.kr>
2016-12-18 19:31:41 +0900

    Add files via upload

--- lab10/ipc_consumer.c
+++ lab10/ipc_consumer.c
@@ -7,41 +7,39 @@
 #define SIZE 20
 
 char *PIPEPATH = "./fifo";  // named pipe 경로
+typedef struct Data{
+	char myId[13];
+	char myName[10];
+	int pid;
+}Data;
 
 int main() {
-	char number[SIZE]; 
-	char name[] = "wooyo";
-	char readBuf[SIZE], writeBuf[SIZE];
-	char pidBuf[SIZE]; // pid를 문자열로 전달할 변수
 	int fp_r, fp_w; // filePointer read,write
 	pid_t pid;
-/*
-	if ( mkfifo(PIPENAME, 0666 != 0 ) {
-		printf("fifo Error\n");
-		return -1;
-	} // fifo 생성 실패
-*/
-	if ( (fp_r = open(PIPEPATH, O_RDONLY)) < 0 || 
-		(fp_w = open(PIPEPATH, O_WRONLY)) < 0 ) {
-		printf("named Pipe open Error\n");
-		return -1;
-	} // 파이프 생성 실패
-
-	memset(readBuf, '\0' , SIZE);
-	memset(writeBuf, '\0', SIZE);
+	Data data;
+	int res;
+	if (access(PIPEPATH, F_OK) == -1) {
+		res = mkfifo(PIPEPATH, 0777);
+		if ( res == -1 ) {
+			printf("fifo Error\n");
+			return -1;
+		}
+	}
 	
 	// 파이프로부터 읽어오기
-	read(fp_r, readBuf, SIZE);
-	strcpy(number, readBuf);
-	read(fp_r, readBuf, SIZE);
-	strcpy(pidBuf, readBuf);
-	printf("producer -> consumer :: %s,%s\n", number, pidBuf);
+	fp_r = open(PIPEPATH, O_RDONLY);
+	read(fp_r, &data, sizeof(data));
+	printf("ipc_consumer에서 .. \n");
+	printf("producer -> consumer :: %d,%s\n", data.pid, data.myId);
 	close(fp_r);
-
+	
+	sleep(1);
 	// 파이프에 쓰기
-	sprintf(pidBuf, "%d", getpid());
-	write(fp_w, name, SIZE);
-	write(fp_w, pidBuf, SIZE);
+	fp_w = open(PIPEPATH, O_WRONLY); 
+	data.pid = getpid();
+	strcpy(data.myName, "wooyo");
+	// 데이터 지정
+	write(fp_w, &data, sizeof(data));
 	close(fp_w);
 	return 0;
 }
--- lab10/ipc_producer.c
+++ lab10/ipc_producer.c
@@ -1,5 +1,4 @@
 #include <fcntl.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -9,73 +8,66 @@
 
 char *PATH = "/home/wooyo/IPC/lab10/ipc_consumer";
 char *PIPEPATH = "./fifo";  // named pipe 경로
+typedef struct Data{
+	char myId[13];
+	char myName[10];
+	int pid;
+}Data;
 
 int main() {
-	char number[] = "2012136096";
-	char name[SIZE];
-	char readBuf[SIZE], writeBuf[SIZE];
 	char pidBuf[SIZE]; // pid를 문자열로 전달할 변수
 	int fp_r, fp_w; // filePointer read,write
 	pid_t pid;
 	int running = 0;
 	char buffer[BUFSIZ]; //최초 start 받는 버퍼
 	int res;
+	Data data;
 	while ( running == 0 ) {
 		printf("Input start Text >> ");
 		fgets(buffer, BUFSIZ, stdin);
 		if ( strncmp(buffer, "start", 5) == 0 ) {
 			running = 1;
 			break;
-		}
+		}	
 		else {
 			printf("입력 오류 \n");
 			memset(buffer, '\0', BUFSIZ);
 		}	
 	}
-/*	
-	res = mkfifo(PIPEPATH, 0666);
-	if ( res != 0 ) {
-		printf("fifo Error\n");
-		return -1;
-	} // fifo 파일 생성 실패
-*/	
-	if ( (fp_r = open(PIPEPATH, O_RDONLY)) < 0 || 
-		(fp_w = open(PIPEPATH, O_WRONLY)) < 0 ) {
-		printf("named Pipe open Error\n");
-		return -1;
-	} // 파이프 생성 실패
-
-	memset(readBuf, '\0', SIZE);
-	memset(writeBuf, '\0', SIZE);
-	printf("aaa");	
-	pid = fork(); 
 	
-	if ( pid < 0 ) {
-		printf("fork Error\n");
-		return -1;
+	if (access(PIPEPATH, F_OK) == -1) {	
+		res = mkfifo(PIPEPATH, 0777);
+		if ( res == -1 ) {
+			printf("fifo Error\n");
+			return -1;
+		} // fifo 파일 생성 실패
 	}
-	if ( pid == 0 ) {
-		execl(PATH, "ipc_consumer", NULL, NULL);
-		printf("execl Error\n");
-	} // 자식 프로세스
-	else { // pid > 0 , 부모프로세스
-		// 파이프에 쓰기 연산
-		sprintf(pidBuf, "%d", getpid());
-		 // 부모의 피드값 string으로 저장
-		write(fp_w, number, SIZE);
-		write(fp_w, pidBuf, SIZE); // 부모 피드값 쓰기
-		sleep(1);
-	}
-	close(fp_w);
-	// 쓰기 파이프 닫고
 
+	pid = fork(); 
+	if ( running == 1 ) { 
+		if ( pid < 0 ) {
+			printf("fork Error\n");
+			return -1;
+		}
+		if ( pid == 0 ) {
+			execl("./ipc_consumer" , "ipc_consumer", NULL, NULL);
+			printf("execl Error\n");
+		} // 자식 프로세스
+		else { //  부모프로세스
+			data.pid = getpid();
+			strcpy(data.myId, "2012136096"); // 쓸 데이터 지정 후, write	
+			fp_w = open(PIPEPATH, O_WRONLY); // 파이프에 쓰기 연산
+			write(fp_w, &data, sizeof(data));
+			close(fp_w); // 쓰기 파이프 닫고
+		}
+	}
 	// 파이프를 통해 자식프로세스에서 온거 읽기 연산
-	read(fp_r, readBuf, SIZE);
-	strcpy(name, readBuf);
-	read(fp_r, readBuf, SIZE);
-	strcpy(pidBuf, readBuf);
-	printf("consumer -> producer ::  %s,%s,",  pidBuf, name);
-	printf("%s,%d\n", number, getpid());
+	fp_r = open(PIPEPATH, O_RDONLY);
+	read(fp_r, &data, sizeof(data));
 	close(fp_r);
+
+	printf("\nipc_producer 에서..\n");	
+	printf("consumer -> producer ::  %d,%s\n", data.pid, data.myName);
+	printf("                         %d,%s\n", getpid(), data.myId);
 	return 0;
 }
